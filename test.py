c105973801db8c850407be66f92ccc4239e84b8f
ultan kearns <ultankearns@gmail.com>
2019-10-08 18:11:59 +0100

    Forgot to reference keras site template used to start project and added tensorflow and researched MNIST so made two variables to be equal to 28 to define image size

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.datasets import mnist
+
+#since MNIST images have 28*28 resolution
+#define image rows and columns to be 28
+image_rows = image_columns = 28
+#template taken from keras site
 model = Sequential()
 model.add(Dense(units=64, activation='relu', input_dim=100))
 model.add(Dense(units=10, activation='softmax'))
