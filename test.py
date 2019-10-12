4e76df3a727bf691c929bb4dccb27b0405814089
ultan kearns <ultankearns@gmail.com>
2019-10-12 20:24:15 +0100

    Read keras docs and implemented code to help classify images

--- test.py
+++ test.py
@@ -2,16 +2,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 #import MNIST from keras.dataset
 from keras.datasets import mnist
+from keras import losses
+from keras import optimizers
 
 #since MNIST images have 28*28 resolution
 #define image rows and columns to be 28
 image_rows = image_columns = 28
-#template taken from keras site
+#this trains and tests the AI with the MNIST DATA
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#template taken from keras site - code creates a Sequential model
 model = Sequential()
 model.add(Dense(units=64, activation='relu', input_dim=100))
 model.add(Dense(units=10, activation='softmax'))
-model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+#optimizer
+model.compile(loss=losses.categorical_crossentropy,
+optimizer=optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
 #load data
 print(mnist.load_data());
