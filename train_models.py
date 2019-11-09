e9d1bc93a550768d0efdcced214b5236be3830b8
ultan kearns <ultankearns@gmail.com>
2019-11-09 19:13:48 +0000

    Prompted user on where they would like to save file imported OS package

--- train_models.py
+++ train_models.py
@@ -6,6 +6,8 @@ from keras import losses
 from keras import optimizers
 import keras as kr
 import matplotlib.pyplot as plt
+import h5py
+import os
 def train():
     #since MNIST images have 28*28 resolution
     #define image rows and columns to be 28
@@ -32,8 +34,26 @@ def train():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     #here we will train the model using training set and testing for x epochs
-    #using 512 batchs for each model
+    #using 256 batchs for each model
     model.fit(x_train,y_train,epochs=6, batch_size=256)
+    #prompt user for path
+    path = input("Please enter directory where you want to save the model: ")
+    try:
+        #check path exists if not make path
+        print(os.path.exists(path) == False)
+        if(os.path.exists(path) == False):
+            os.mkdir(path)
+        os.chdir(path)
+        #save model
+        model.save("trained.h5")
+        print("Model saved to " + path)
+    except:
+        print(path + " not found please make directory using mkdir")
+        menu();
+
+def prediction():
+    print("Predict......")
+
 def menu():
     print("""Python App to classify image using the MNIST Dataset and Keras - Ultan Kearns G00343745""")
     print("1. To train Model")
@@ -46,4 +66,5 @@ def menu():
         print("not implemented yet")
     else:
         print("Not a valid option")
+        menu();
 menu()
